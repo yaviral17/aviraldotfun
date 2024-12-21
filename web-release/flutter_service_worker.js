07dc0c4fbafb45333cf0aafe4902902d55788a3b
yaviral17 <yaviral17@gmail.com>
2024-12-22 02:29:41 +0530

    fix: update service worker version and resource hashes for web release

--- web-release/flutter_service_worker.js
+++ web-release/flutter_service_worker.js
@@ -2255,7 +2255,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c8dd03ddd7b28da955ef78684bed80b7
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "38d6b65fb3410656f3770c6158f346ed",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "b4054c96668862a0caaf980cbb974ef1",
+"flutter_bootstrap.js": "720ab465a789c77fa393faaaac0d6a51",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
